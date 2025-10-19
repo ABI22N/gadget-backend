a13283b2ef5f86079cc91a7cdbb37fc50728a0f5
Abin Alias <abinalias004@gmail.com.com>
2025-10-19 23:32:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// import the json server
+
+const JSONServer = require('json-server') //returns an express server
+
+// create server for running json file
+
+const gadgets = JSONServer.create()
+
+// 3. create middleware
+
+const middleware=JSONServer.defaults()
+
+//6.import db.json file
+
+const router=JSONServer.router("db.json")
+
+//4.defime port to run the server
+
+const PORT=3000|| process.env.PORT
+//5.use middleware
+
+gadgets.use(middleware)
+//7.use router
+
+rBuilder.use(router)
+
+//8.tell the server to listen for the client
+
+gadgets.listen(PORT,()=>{
+    console.log(`resume builder starts at PORT number ${PORT}`);
+    
+})
